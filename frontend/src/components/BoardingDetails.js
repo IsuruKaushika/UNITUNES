aa7efdf5fe4c8643a65fb70baa255b8c5218b58e
ACWeerathunga <aweerathunga20@gmail.com>
2025-08-25 10:18:51 +0530

    Fix casing of 'uniTunes' to 'UniTunes' in CustomLogo component

--- frontend/src/components/BoardingDetails.js
+++ frontend/src/components/BoardingDetails.js
@@ -20,7 +20,7 @@ const CustomLogo = ({ onClick, className = "" }) => (
           <div className="absolute -top-1 -right-1 w-3 h-3 bg-green-400 rounded-full border-2 border-white"></div>
         </div>
         <div className="text-white font-bold text-lg">
-          uniTunes
+          UniTunes
         </div>
       </div>
     </div>
